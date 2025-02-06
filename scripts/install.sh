71c318d932ebcbc4822df1fda1ea17e247272c99
vesperrin <erin.marilleau@gmail.com>
2025-02-06 19:09:08 +0100

    make scripts executable

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,13 +9,13 @@ REMOTE_PATH=https://raw.githubusercontent.com/vesperrin/ubuntu-studio-setup/refs
 # shellcheck disable=SC1090
 source <(curl -fsSL "${REMOTE_PATH}/install.sh")
 
-curl -sSL "${REMOTE_PATH}/install-syncthing.sh" | sudo bash -s --
+# curl -sSL "${REMOTE_PATH}/install-syncthing.sh" | sudo bash -s --
 
 curl -sSL "${REMOTE_PATH}/install-wine.sh" | sudo bash -s --
-curl -sSL "${REMOTE_PATH}/install-wine-mono.sh" | sudo bash -s --
-curl -sSL "${REMOTE_PATH}/install-yabridge.sh" | sudo bash -s --
+# curl -sSL "${REMOTE_PATH}/install-wine-mono.sh" | sudo bash -s --
+# curl -sSL "${REMOTE_PATH}/install-yabridge.sh" | sudo bash -s --
 
-curl -sSL "${REMOTE_PATH}/install-studio-one.sh" | sudo bash -s --
-curl -sSL "${REMOTE_PATH}/install-vcv-rack.sh" | sudo bash -s --
-curl -sSL "${REMOTE_PATH}/install-pulse.sh" | sudo bash -s --
-curl -sSL "${REMOTE_PATH}/install-cableguys-shaperbox.sh" | sudo bash -s --
+# curl -sSL "${REMOTE_PATH}/install-studio-one.sh" | sudo bash -s --
+# curl -sSL "${REMOTE_PATH}/install-vcv-rack.sh" | sudo bash -s --
+# curl -sSL "${REMOTE_PATH}/install-pulse.sh" | sudo bash -s --
+# curl -sSL "${REMOTE_PATH}/install-cableguys-shaperbox.sh" | sudo bash -s --
